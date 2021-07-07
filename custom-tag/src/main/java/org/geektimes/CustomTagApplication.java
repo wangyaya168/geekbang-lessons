c22dca9da7f68531a9f527685a5119ea0b7c364a
wangya <wangyaya15088648349@126.com>
2021-07-07 08:56:34 +0800

    aa

---
+++ custom-tag/src/main/java/org/geektimes/CustomTagApplication.java
@@ -0,0 +1,13 @@
+package org.geektimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomTagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomTagApplication.class, args);
+    }
+
+}
