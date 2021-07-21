b2f96f74f65d71652089ca0ecca97c4f153d2d89
wangya <wangyaya15088648349@126.com>
2021-07-21 16:18:51 +0800

    修改

---
+++ Geek-Bang-Java/MicroProfile/src/main/java/org/geektimes/MicroProfileApplication.java
@@ -0,0 +1,15 @@
+package org.geektimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangya
+ * @date 2021-07-21 16:14
+ */
+@SpringBootApplication
+public class MicroProfileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroProfileApplication.class,args);
+    }
+}
